12ed61bd5574d47faa5c28e9b3f9fdfcba8451bd
SharifMAbdullah <bsse1211@iit.du.ac.bd>
2023-07-16 15:53:14 +0600

    basics done, notification to do

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const Post = require("../models/Post");
 
-router.get("/", async (req, res) => {
-  const result = await User.find({});
-  //console.log(result);
-  res.json(result);
+router.post("/post", async (req, res) => {
+  const newPost = new Post({
+    description: req.body.description,
+    image: req.body.image,
+  });
+});
+
+router.get("/post", async (req, res) => {
+  //find latest post of all users
+  const posts = await Post.find().sort({ createdAt: -1 });
+  res.json(posts);
 });
 
 module.exports = router;
